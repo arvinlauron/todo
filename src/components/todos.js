b7d3b758f60e0bcf7aa1eaad288a7d77366c13bf
arvinlauron <arvinglauron@gmail.com>
2022-01-22 11:36:32 +0800

    first init

---
+++ src/components/todos.js
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+import Todo from "./todo";
+import AddTodo from "./addtodo";
+
+export default class Todos extends Component {
+  constructor(props){
+  super(props);
+  this.state = {
+    addTodoValue: "",
+    todos: [
+      {
+        id: 1,
+        value: "Assignment",
+        isDone: false,
+      },
+
+      {
+        id: 2,
+        value: "Todo App",
+        isDone: true,
+      },
+
+      {
+        id: 3,
+        value: "Post",
+        isDone: false,
+      },
+    ]
+  };
+}
+
+  handleDelete = (todo) => {
+
+    const todos = this.state.todos.filter((t) => {
+      return t.id !== todo;
+    });
+    
+    this.setState({ todos });
+  };
+
+  handleDone = (todo) => {
+    const todos = [...this.state.todos];
+    todos.map((t) => {
+      if (t.id === todo.id) {
+        t.isDone = !t.isDone;
+      }
+      return t;
+    });
+    this.setState({ todos });
+  };
+  getTime = () =>{
+    let d = new Date();
+    var n = d.getTime();
+    return n;
+  }
+
+  addNewTodo = (value) => {
+    if (value) {
+      const todos = [...this.state.todos];
+      todos.push({
+        id: this.getTime(),
+        value: value,
+        isDone: false,
+      });
+      this.setState({ addTodoValue: "", todos });
+    } else {
+      alert("Please Add Todo Text");
+    }
+  }
+
+  render() {
+    return (
+      <table className="table">
+        <tbody>
+          {this.state.todos.map((todo, index) => (
+            <tr key={todo.id}>
+              <Todo
+                index={index + 1}
+                todo={todo}
+                fooDelete={this.handleDelete}
+                fooDoneDone={this.handleDone}
+              />
+            </tr>
+          ))}
+          <tr>
+            <td colSpan="4" className="text-center">
+              <AddTodo
+                fooAddTodo={this.addNewTodo}
+                addTodoValue={this.state.addTodoValue}
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
+
+
