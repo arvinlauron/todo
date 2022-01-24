2825671b6748cbc2b3283b69be8761eb61f66334
arvinlauron <arvinglauron@gmail.com>
2022-01-24 11:25:45 +0800

    modified empty array warning

--- src/components/todos.js
+++ src/components/todos.js
@@ -2,41 +2,41 @@ import React, { Component } from "react";
 import Todo from "./todo";
 import AddTodo from "./addtodo";
 
-
 export default class Todos extends Component {
-  constructor(props){
-  super(props);
-  this.state = {
-    isActive:false,
-    addTodoValue: "",
-    todos: [
-      {
-        id: 1,
-        value: "Assignment",
-        isDone: false,
-      },
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false,
 
-      {
-        id: 2,
-        value: "Todo App",
-        isDone: true,
-      },
+      addTodoValue: "",
+      todos: [
+        {
+          id: 1,
+          value: "Assignment",
+          isDone: false,
+        },
 
-      {
-        id: 3,
-        value: "Post",
-        isDone: false,
-      },
-    ]
-  };
-}
+        {
+          id: 2,
+          value: "Todo App",
+          isDone: true,
+        },
+
+        {
+          id: 3,
+          value: "Post",
+          isDone: false,
+        },
+      ],
+    };
+  }
 
   handleDelete = (todo) => {
     const todos = this.state.todos.filter((t) => {
       return t.id !== todo;
     });
-    if(!todos.length){
-      this.setState({isActive:true})
+    if (!todos.length) {
+      this.setState({ isActive: true });
     }
     this.setState({ todos });
   };
@@ -51,11 +51,11 @@ export default class Todos extends Component {
     });
     this.setState({ todos });
   };
-  getTime = () =>{
+  getTime = () => {
     let d = new Date();
     var n = d.getTime();
     return n;
-  }
+  };
 
   addNewTodo = (value) => {
     if (value) {
@@ -66,41 +66,42 @@ export default class Todos extends Component {
         isDone: false,
       });
       this.setState({ addTodoValue: "", todos });
-      this.setState({isActive:false})
+      this.setState({ isActive: false });
     } else {
       alert("Please Add Todo Text");
     }
-  }
+  };
 
   render() {
     return (
-      
-      <table className="table">
-        {this.state.isActive? <div className="alert alert-danger">No to do!</div>: null} 
-        <tbody>
-          
-          {this.state.todos.map((todo, index) => (
-            <tr key={todo.id}>
-              <Todo
-                index={index + 1}
-                todo={todo}
-                fooDelete={this.handleDelete}
-                fooDoneDone={this.handleDone}
-              />
+      <div>
+        {this.state.isActive ? (
+          <div className="alert alert-danger">No to do!</div>
+        ) : null}
+
+        <table className="table">
+          <tbody>
+            {this.state.todos.map((todo, index) => (
+              <tr key={todo.id}>
+                <Todo
+                  index={index + 1}
+                  todo={todo}
+                  fooDelete={this.handleDelete}
+                  fooDoneDone={this.handleDone}
+                />
+              </tr>
+            ))}
+            <tr>
+              <td colSpan="4" className="text-center">
+                <AddTodo
+                  fooAddTodo={this.addNewTodo}
+                  addTodoValue={this.state.addTodoValue}
+                />
+              </td>
             </tr>
-          ))}
-          <tr>
-            <td colSpan="4" className="text-center">
-              <AddTodo
-                fooAddTodo={this.addNewTodo}
-                addTodoValue={this.state.addTodoValue}
-              />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
-
-
